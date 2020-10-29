92ebae6892c7dd882c8924d3596b1606e9d3b9b8
imheidimarais <imheidimarais@gmail.com>
2020-10-29 17:17:53 +0100

    Module 2 tutorial and updated function.

--- functions/module2.py
+++ functions/module2.py
@@ -10,6 +10,18 @@ from tabulate import tabulate
 from statsmodels.stats.multicomp import pairwise_tukeyhsd
 
 def ANOVA(dataset,independent,dependent,confidence = 0.95):
+    """ Calculates the ANOVA for a given dataset and prints an ANOVA table
+        and results of post hoc test if test was performed.
+
+    Args:  # 'arguments', or inputs to the function
+        dataset (pandas.DataFrame): The data
+        independent (string): The name of the independent column.
+        dependent (string): The name of the dependent column.
+        confidence (float): The desired confidence level for the ANOVA.
+
+    Returns:
+        None.
+    """
     groups = pandas.unique(dataset[independent])
     k = len(groups)  # number of groups
     n = len(dataset[dependent])  # number of dependent data points
@@ -23,7 +35,7 @@ def ANOVA(dataset,independent,dependent,confidence = 0.95):
     # x_dd = sum over i and j x_ij
     # x_id = sum over j x_ij
     # x_dj = sum over i x_ij
-    # where i is the idependent variable and j is the dependent variable
+    # where i is the independent variable and j is the dependent variable
 
     x_dd = sum(dataset[dependent])
     CF = (x_dd**2)/n
@@ -32,10 +44,10 @@ def ANOVA(dataset,independent,dependent,confidence = 0.95):
 
     SSbetween = 0
     for i in groups:
-      group_data = dataset.loc[dataset[independent]==i]
-      n_i = len(group_data[dependent])
-      x_id = sum(group_data[dependent])
-      SSbetween += (x_id**2)/n_i
+        group_data = dataset.loc[dataset[independent]==i]
+        n_i = len(group_data[dependent])
+        x_id = sum(group_data[dependent])
+        SSbetween += (x_id**2)/n_i
 
     SSbetween = SSbetween - CF # so^2 - s^2
 
@@ -45,7 +57,7 @@ def ANOVA(dataset,independent,dependent,confidence = 0.95):
     MSwithin = SSwithin/DFwithin
 
     F = MSbetween/MSwithin
-    p = sstats.f.sf(F, DFbetween, DFwithin)
+    p = stats.f.sf(F, DFbetween, DFwithin)
 
     print(tabulate([['Between', DFbetween, SSbetween, MSbetween, F],
                     ['Within', DFwithin, SSwithin, MSwithin, ' '],
@@ -55,7 +67,7 @@ def ANOVA(dataset,independent,dependent,confidence = 0.95):
     print('\n')
     alpha = 1-confidence
     if p<alpha:
-      print("Reject null-hypothesis: There are statistical differences present.")
-      print(pairwise_tukeyhsd(dataset[dependent], dataset[independent],alpha=alpha))
+        print("Reject null-hypothesis: There are statistical differences present.")
+        print(pairwise_tukeyhsd(dataset[dependent], dataset[independent],alpha=alpha))
     else:
-      print("Accept null-hypothesis: There are no statistical differences present.")
+        print("Fail to reject the null-hypothesis: There are no statistical differences present at this level of significance.")
